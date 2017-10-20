0463f640f10271d984b8684c6c3a0fb73faea848
LeoXugu <robinleoknauth@gmail.com>
2017-10-20 16:40:03 -0700

    W3D5

--- W3D5/skeleton 7/lib/01_sql_object.rb
+++ W3D5/skeleton 7/lib/01_sql_object.rb
@@ -5,53 +5,156 @@ require 'active_support/inflector'
 
 class SQLObject
   def self.columns
-    # ...
+    return @columns if @columns
+    @columns = DBConnection.execute2(<<-SQL).first
+      SELECT
+        *
+      FROM
+        #{table_name}
+      LIMIT
+        1
+
+
+
+    SQL
+    @columns = @columns.map(&:to_sym)
+
+
   end
 
   def self.finalize!
+
+    columns.each do |col|
+      define_method(col) do
+        attributes[col]
+      end
+      define_method("#{col}=") do |new_value|
+        attributes[col] = new_value
+      end
+    end
+
+    # attributes.each do |k, v|
+    #   define_method(k) do
+    #     instance_variable_get("@#{k}")
+    #   end
+      # define_method("#{k}=") do |v|
+      #   instance_variable_set("@#{k}", v)
+      # end
+    # end
+
   end
 
   def self.table_name=(table_name)
-    # ...
+    @table_name = table_name
   end
 
   def self.table_name
-    # ...
+    @table_name || self.name.underscore.pluralize
   end
 
   def self.all
-    # ...
+
+    res = DBConnection.execute(<<-SQL)
+      SELECT
+        #{table_name}.*
+      FROM
+        #{table_name}
+    SQL
+
+    parse_all(res)
+
   end
 
   def self.parse_all(results)
-    # ...
+    # thing = self.class.new
+    # results.map do |prop|
+    #   thing.se
+    # end
+
+
+    results.map { |res| self.new(res) }
   end
 
   def self.find(id)
-    # ...
+    res = DBConnection.execute(<<-SQL, id)
+      SELECT
+        #{table_name}.*
+      FROM
+        #{table_name}
+      where
+        id = ?
+    SQL
+
+    parse_all(res).first
   end
 
   def initialize(params = {})
-    # ...
+    # params.each do |k, v|
+    #   define_method(k) do
+    #     instance_variable_get("@#{k}")
+    #   end
+    #   define_method("#{k}=") do |v|
+    #     instance_variable_set("@#{k}", v)
+    #   end
+    # end
+
+    params.each do |k, v|
+      parms_name = k.to_sym
+      if !self.class.columns.include?(parms_name)
+        raise "unknown attribute '#{k}'"
+      else
+        send("#{parms_name}=", v)
+      end
+    end
   end
 
   def attributes
-    # ...
+    @attributes ||= {}
+
   end
 
   def attribute_values
-    # ...
+    self.class.columns.map { |att| send(att) }
   end
 
   def insert
-    # ...
+    columns = self.class.columns.drop(1)
+    col_names = columns.map(&:to_s).join(", ")
+    question_marks = (["?"] * columns.count).join(", ")
+
+    DBConnection.execute(<<-SQL, *attribute_values.drop(1))
+      INSERT INTO
+        #{self.class.table_name} (#{col_names})
+      VALUES
+        (#{question_marks})
+    SQL
+
+    self.id = DBConnection.last_insert_row_id
   end
 
   def update
-    # ...
+    update_line = self
+      .class
+      .columns
+      .map { |att| "#{att} = ?"}
+      .join(", ")
+
+      DBConnection.execute(<<-SQL, *attribute_values, id)
+        UPDATE
+          #{self.class.table_name}
+        SET
+          #{update_line}
+        WHERE
+          #{self.class.table_name}.id = ?
+      SQL
+
   end
 
   def save
-    # ...
+    if id.nil?
+      insert
+    else
+      update
+    end
   end
 end
--- W3D5/skeleton 7/lib/03_associatable.rb
+++ W3D5/skeleton 7/lib/03_associatable.rb
@@ -10,41 +10,68 @@ class AssocOptions
   )
 
   def model_class
-    # ...
+    @class_name.constantize
   end
 
   def table_name
-    # ...
+    model_class.table_name
   end
 end
 
 class BelongsToOptions < AssocOptions
   def initialize(name, options = {})
-    # ...
+    defaults = {
+      foreign_key: "#{name}_id".to_sym,
+      class_name: name.to_s.camelcase,
+      primary_key: :id
+    }
+
+    defaults.keys.each do |key|
+      self.send("#{key}=", options[key] || defaults[key])
+    end
   end
 end
 
 class HasManyOptions < AssocOptions
   def initialize(name, self_class_name, options = {})
-    # ...
+    defaults = {
+      foreign_key: "#{self_class_name.underscore}_id".to_sym,
+      class_name: name.to_s.singularize.camelcase,
+      primary_key: :id
+    }
+
+    defaults.keys.each do |key|
+      self.send("#{key}=", options[key] || defaults[key])
+    end
   end
 end
 
 module Associatable
   # Phase IIIb
+
   def belongs_to(name, options = {})
-    # ...
+    self.assoc_options[name] = BelongsToOptions.new(name, options)
+    define_method(name) do
+      options = self.class.assoc_options[name]
+      key = self.send(options.foreign_key)
+      options
+        .model_class
+        .where(options.primary_key => key)
+        .first
+    end
   end
 
+
   def has_many(name, options = {})
-    # ...
+
   end
 
   def assoc_options
-    # Wait to implement this in Phase IVa. Modify `belongs_to`, too.
+    @assoc_opts ||= {}
   end
 end
 
 class SQLObject
-  # Mixin Associatable here...
+  extend Associatable
+  # include Associatable
 end
