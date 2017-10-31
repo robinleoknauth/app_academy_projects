0d4ae7728e321638fa21cf43e1b1442a0f996209
LeoXugu <robinleoknauth@gmail.com>
2017-10-30 18:00:18 -0700

    redo user tests

--- W5D1/goal_app/spec/factories/users.rb
+++ W5D1/goal_app/spec/factories/users.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :user do
-    
+    email {Faker::Internet.email}
+    session_token "MyString" 
+    password_digest "MyString"
   end
 end
--- W5D1/goal_app/spec/models/user_spec.rb
+++ W5D1/goal_app/spec/models/user_spec.rb
@@ -2,11 +2,14 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
 
-
-
-  it { should validate_presence_of(:username) }
-  it { should validate_uniqueness_of(:username) }
+  RSpec.describe User, type: :model do
 
 
 
+    it { should validate_presence_of(:username) }
+    it { should validate_uniqueness_of(:username) }
+
+
+
+  end
 end
