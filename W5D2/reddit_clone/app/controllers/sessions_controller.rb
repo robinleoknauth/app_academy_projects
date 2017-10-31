c3bc4cf8dbdf074601030f22e6540b4211ec3276
LeoXugu <robinleoknauth@gmail.com>
2017-10-31 11:31:36 -0700

    complete auth

--- W5D2/reddit_clone/app/controllers/sessions_controller.rb
+++ W5D2/reddit_clone/app/controllers/sessions_controller.rb
@@ -1,10 +1,24 @@
 class SessionsController < ApplicationController
   def create
+    user = User.find_by_credentials(
+      params[:user][:username],
+      params[:user][:password],
+
+    )
+
+    if user
+      login(user)
+
+    else
+      flash.now[:errors] = ["Invalid Username or Password"]
+      redirect_to new_session_url
+    end
   end
 
   def new
   end
 
   def destroy
+    logout
   end
 end
