a16a889f4796788fbdb8221deb601f7b8444468b
LeoXugu <robinleoknauth@gmail.com>
2017-10-30 18:09:59 -0700

    fixes

--- W5D1/goal_app/spec/models/user_spec.rb
+++ W5D1/goal_app/spec/models/user_spec.rb
@@ -1,15 +1,14 @@
 require 'rails_helper'
 
+
+
 RSpec.describe User, type: :model do
 
-  RSpec.describe User, type: :model do
+
+
+  it { should validate_presence_of(:username) }
+  it { should validate_uniqueness_of(:username) }
 
 
 
-    it { should validate_presence_of(:username) }
-    it { should validate_uniqueness_of(:username) }
-
-
-
-  end
 end
