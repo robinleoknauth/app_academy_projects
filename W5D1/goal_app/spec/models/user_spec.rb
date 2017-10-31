82e335f5145f34feb4a35d20fdf427f9a3378a57
LeoXugu <robinleoknauth@gmail.com>
2017-10-30 18:12:25 -0700

    fixes

--- W5D1/goal_app/spec/models/user_spec.rb
+++ W5D1/goal_app/spec/models/user_spec.rb
@@ -4,11 +4,28 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
 
+  #can't get FactoryBot to work :(
+
+  subject(:user) do
+    User.create!(username: "leo", password: "123456")
+  end
 
 
   it { should validate_presence_of(:username) }
   it { should validate_uniqueness_of(:username) }
 
 
+   describe "password encryptioin" do
+     it "doesnt save password to the database" do
+       User.create!(username: "leo", password: "123456")
+       user = User.find_by_username("leo")
+       expect(user.password).not_to be("123456")
+     end
+
+     it "encrypt password using BCrypt" do
+       expect(BCrypt::Password).to receive(:create)
+       User.new(username: "leo", password: "123456")
+     end
+   end
 
 end
